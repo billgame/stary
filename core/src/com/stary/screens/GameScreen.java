c77c801d4a1530e957b71c8385ac94813424b375
hopeful <hopeful_world@163.com>
2015-09-24 07:32:49 +0800

    Revert "Revert "core android""
    This reverts commit b47b57bd14e04f41db446953aa81fe76341162d4.
    
    Conflicts:
    	android/assets/maps/testmap/scene_test.tmx
    	core/src/com/stary/ems/systems/SkeletonSystem.java
    	core/src/com/stary/ems/systems/SpineRenderSystem.java
    	core/src/com/stary/screens/GameScreen.java
    	core/src/com/stary/utils/Box2dUtil.java

--- core/src/com/stary/screens/GameScreen.java
+++ core/src/com/stary/screens/GameScreen.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.stary.screens;
 
 import com.badlogic.ashley.core.Engine;
@@ -330,3 +331,336 @@ public class GameScreen extends BasicScreen{
 		
 	}
 }
+=======
+package com.stary.screens;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.PolylineMapObject;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Affine2;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.ChainShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.utils.Box2DBuild;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.FloatArray;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.esotericsoftware.spine.AnimationState;
+import com.esotericsoftware.spine.AnimationStateData;
+import com.esotericsoftware.spine.Skeleton;
+import com.esotericsoftware.spine.SkeletonBounds;
+import com.esotericsoftware.spine.SkeletonData;
+import com.esotericsoftware.spine.SkeletonJson;
+import com.esotericsoftware.spine.SkeletonRenderer;
+import com.esotericsoftware.spine.SkeletonRendererDebug;
+import com.esotericsoftware.spine.Skin;
+import com.esotericsoftware.spine.Slot;
+import com.esotericsoftware.spine.attachments.AtlasAttachmentLoader;
+import com.esotericsoftware.spine.attachments.Attachment;
+import com.esotericsoftware.spine.attachments.AttachmentLoader;
+import com.esotericsoftware.spine.attachments.BoundingBoxAttachment;
+import com.esotericsoftware.spine.attachments.MeshAttachment;
+import com.esotericsoftware.spine.attachments.RegionAttachment;
+import com.esotericsoftware.spine.attachments.SkinnedMeshAttachment;
+import com.stary.data.Assets;
+import com.stary.data.GameData;
+import com.stary.ems.components.SkeletonBox2dComponent;
+import com.stary.ems.systems.SkeletonSystem;
+import com.stary.ems.systems.SpineRenderSystem;
+import com.stary.utils.AshleyUtil;
+import com.stary.utils.Box2dBoundingBoxAttachment;
+import com.stary.utils.Box2dUtil;
+import com.stary.utils.TMXUtil;
+
+import static com.stary.data.GameData.*;
+/**
+ * a Odin-like
+ * side-scrolling action game
+ * 
+ * 
+ * @author billzhu
+ *
+ */
+public class GameScreen extends BasicScreen{
+//	Camera uiCamera;
+//	Camera worldCamera;
+	float pw=GameData.viewPortPixelWidth, ph=GameData.viewPortPixelHeight;
+	float mw=GameData.viewPortMeterWidth;
+	float screenRatio=GameData.screenRatio;//1.77777
+	float pxToPhy=GameData.pxToPhy;//how much meter per pixel , multiply
+	
+	Engine ashleyEngine;
+	
+	PolygonSpriteBatch polygonSpriteBatch;
+	
+	Box2DDebugRenderer box2dDebugRenderer;
+	ShapeRenderer sr=new ShapeRenderer();
+	
+	SpineRenderSystem spineRenderSystem;
+
+	SkeletonRendererDebug debugRenderer;
+	SkeletonRenderer skeletonRenderer;
+	SkeletonJson json;
+	SkeletonBounds skeletonBounds=new  SkeletonBounds();
+	
+	World box2dWorld;
+	
+	Array<Body> bodies;
+//	Array<Skeleton> skeletons;
+	
+	TiledMap tiledMap;
+	Stage physicalStage,pxStage;
+	SpriteBatch phyBatch;
+	public GameScreen(Game game) {
+		super(game);
+	}
+	@Override
+	public void show() {
+		
+		polygonSpriteBatch = new PolygonSpriteBatch(); // Required to render meshes. SpriteBatch can't render meshes.
+		skeletonRenderer = new SkeletonRenderer();
+		skeletonRenderer.setPremultipliedAlpha(true); // PMA results in correct blending without outlines.
+		debugRenderer = new SkeletonRendererDebug();
+//		debugRenderer.setPremultipliedAlpha(true); // PMA results in correct blending without outlines.
+		debugRenderer.setBoundingBoxes(false);
+		debugRenderer.setRegionAttachments(false);
+		debugRenderer.setMeshTriangles(false);
+		
+		box2dDebugRenderer=new Box2DDebugRenderer();
+		box2dWorld=GameData.box2dWorld;
+		
+		//(8.5  5.1)(32, 19.2f)
+		physicalStage =new Stage(new FitViewport(mw, mw/screenRatio,cameraMap));
+		//(800,480)
+		pxStage= new Stage(new FitViewport(pw, ph), physicalStage.getBatch());
+		phyBatch=(SpriteBatch) physicalStage.getBatch();
+		
+		GameData.instance.pxStage= pxStage;
+		GameData.instance.phyStage= physicalStage;
+		super.show();
+
+
+		ashleyEngine=GameData.ashley;
+		
+		Family f=Family.all(SkeletonBox2dComponent.class).get();
+		SkeletonSystem skeletonSystem= new SkeletonSystem(f);
+		spineRenderSystem=new SpineRenderSystem(f);
+		ashleyEngine.addEntityListener(Family.all(SkeletonBox2dComponent.class).get(),skeletonSystem);
+		ashleyEngine.addSystem(skeletonSystem);
+		ashleyEngine.addSystem(spineRenderSystem);
+		
+		loadMap();
+//		System.out.println(tiledMap.getLayers().get("physicalLayer"));
+		createPhyLayer(tiledMap.getLayers());
+		initEntityBox2dSpine(tiledMap.getLayers());
+	}
+	@Override
+	public void render(float delta) {
+
+		box2dWorld.step(delta, 6, 2);
+		
+		ashley.update(delta);
+		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		super.render(delta);
+		SpriteBatch pxBatch=(SpriteBatch) pxStage.getBatch();
+		pxBatch.setProjectionMatrix(pxStage.getCamera().combined);
+		pxBatch.begin();
+		//draw map layers
+		for (int i = 0; i < layers.getCount(); i++) {
+			MapLayer layer=layers.get(i);
+			//if it has some object ,it's an objectslayer
+//			if (!layer.getName().equals("objectLayer_bill_close1"))
+//				continue;
+			if(layer.getName().startsWith("physicalLayer")){
+				continue;
+			}
+			MapObjects objs=layer.getObjects();
+			for (int j = 0; j < objs.getCount(); j++) {
+				MapObject obj=objs.get(j);
+//				if(j==1)break;
+//				if (!(obj.getName().equals("mountain1"))) continue;
+				if(!obj.isVisible()) continue;
+				if (obj instanceof TextureMapObject) {
+					TextureMapObject texObj=(TextureMapObject)obj;
+					TextureRegion region=texObj.getTextureRegion();
+//					float w=texObj.getProperties().get("width",0.0f,Float.class);//scaled width
+//					float h=texObj.getProperties().get("height",0.0f,Float.class);//scaled height
+					pxBatch.draw(region,texObj.getX(), texObj.getY(),
+							texObj.getOriginX(), texObj.getOriginY(), 
+							region.getRegionWidth(),region.getRegionHeight(),
+							texObj.getScaleX(),texObj.getScaleY(),
+							-texObj.getRotation());
+					//same as above...[bill]
+//					pxBatch.draw(region,texObj.getX(), texObj.getY(),
+//							texObj.getOriginX(), texObj.getOriginY(), 
+//							w,h,1,1,-texObj.getRotation());
+					//same as above...[bill]
+//					Affine2 a=new Affine2();
+//					a.translate(texObj.getX(),texObj.getY());//second translates 
+//					a.rotate(-texObj.getRotation());	//first rotates at world orgin
+//					pxBatch.draw(region, w, h, a);
+				}
+			}
+		}
+		pxBatch.end();
+		
+		spineRenderSystem.update(delta);
+		
+	}
+	MapLayers layers;
+	public void loadMap(){
+		tiledMap=new TmxMapLoader().load(testmap);
+		layers=tiledMap.getLayers();
+		for (int i = 0; i < layers.getCount(); i++) {
+			MapLayer layer=layers.get(i);
+			System.out.println(layer.getName()+" opacity:"+layer.getOpacity()
+			+" isMaplayer:"+(layer.getClass()==MapLayer.class)+", "
+			+"x objects:"+layer.getObjects().getCount()+" class:"+ layer.getClass().getSimpleName());
+		}
+		MapLayer layer=layers.get("objectLayer_bill_close2");
+		MapObjects objects=layer.getObjects();
+	}
+	
+
+	private void initEntityBox2dSpine(MapLayers layers2) {
+		MapLayer actorLayer=layers.get("actors");
+		for(MapObject actorObj:actorLayer.getObjects()){
+			System.out.println(actorObj.getName());
+			String actorName=actorObj.getName();
+			//position in world(meter) [bill]
+			Vector2 pos=TMXUtil.getPosition(actorObj, pxToPhy);
+			Entity actor=
+					AshleyUtil.createActor()
+					.add(new SkeletonBox2dComponent(box2dWorld,actorName,pos.x,pos.y,pxToPhy));
+			ashleyEngine.addEntity(actor);
+		}
+	}
+	
+	AnimationState state;
+	public Skeleton initSpineActor(MapObject actor){
+		String name=actor.getName();
+		TextureAtlas atlas=new TextureAtlas(character+name+".atlas");
+		AttachmentLoader al=new AtlasAttachmentLoader(atlas) {
+			@Override
+			public BoundingBoxAttachment newBoundingBoxAttachment(Skin skin, String name) {
+				return new Box2dBoundingBoxAttachment(name);
+			}
+		};
+		json = new SkeletonJson(al);
+		json.setScale(pxToPhy);
+		System.out.println("create skeletion in scale "+pxToPhy);
+		SkeletonData skeletonData = json.readSkeletonData(Gdx.files.internal(character+name+".json"));
+		Skeleton skeleton=new Skeleton(skeletonData);
+		Vector2 pos=TMXUtil.getPosition(actor,pxToPhy);//get position(in meter) 
+		skeleton.setPosition(pos.x, pos.y);
+		skeleton.setSkin("goblin");
+//		skeleton.setToSetupPose();
+		skeleton.updateWorldTransform();
+		System.out.println("actor position : "+skeleton.getX()+","+skeleton.getY());
+		
+		AnimationStateData stateData = new AnimationStateData(skeletonData); // Defines mixing (crossfading) between animations.
+//		stateData.setMix("run", "jump", 0.2f);
+//		stateData.setMix("jump", "run", 0.2f);
+
+		state = new AnimationState(stateData); // Holds the animation state for a skeleton (current animation, time, etc).
+		state.setTimeScale(0.5f); // Slow all animations down to 50% speed.
+
+		// Queue animations on track 0.
+		state.setAnimation(0, "walk", true);
+		
+//		skeletonBounds.update(skeleton, false);
+//		skeletonBounds.getPolygons();
+		return skeleton;
+//		for(FloatArray a:skeletonBounds.getPolygons()){
+//			BodyDef boxBodyDef = new BodyDef();
+//			boxBodyDef.type = BodyType.StaticBody;
+//			attachment.body = world.createBody(boxBodyDef);
+//			attachment.body.createFixture(boxPoly, 1);
+//
+//			boxPoly.dispose();
+//		}
+//		for (Slot slot : skeleton.getSlots()) {
+//			if (!(slot.getAttachment() instanceof BoundingBoxAttachment)) continue;
+//			BoundingBoxAttachment attachment = (BoundingBoxAttachment)slot.getAttachment();
+//
+//			PolygonShape boxPoly = new PolygonShape();
+//			boxPoly.setAsBox(attachment.getWidth() / 2 * attachment.getScaleX(),
+//				attachment.getHeight() / 2 * attachment.getScaleY(), vector.set(attachment.getX(), attachment.getY()),
+//				attachment.getRotation() * MathUtils.degRad);
+//		}
+		
+	}
+	public void createPhyLayer(MapLayers layers){
+		bodies=new Array<Body>();
+		MapLayer box2dLayer=layers.get("physicalLayer");
+		for(MapObject obj:box2dLayer.getObjects()){
+			if (obj instanceof TextureMapObject) continue;
+			Shape shape = null;
+			BodyDef bodyDef=new BodyDef();
+			String type=
+					obj.getProperties().get(bodyType,staticBody, String.class);//TODO staticBody emun.valueOf()
+			if(staticBody.equals(type)){
+				bodyDef.type=BodyDef.BodyType.StaticBody;
+			}else if(dynamicBody.equals(type)){
+				bodyDef.type=BodyDef.BodyType.DynamicBody;
+			}else{
+				bodyDef.type=BodyDef.BodyType.KinematicBody;
+			}
+			shape=Box2dUtil.getShape(obj, pxToPhy);
+			if ("physicalLand".equals(obj.getName())) {
+				GameData.instance.land=Box2dUtil.getVertices(obj, pxToPhy);
+			}
+			/*
+			 * [bill]
+			 * TODO terrian material will be settingin TMX map file(poperty terrian：xxx)
+			 * for example: terrian:mountain terrian:snow terrian:green terrian:sand
+			 */
+			FixtureDef fixtureDef=new FixtureDef();
+			fixtureDef.shape=shape;
+			//TODO [bill]terrian density,friction...will be setting in database manually
+			fixtureDef.density=0.8f;
+			fixtureDef.friction=0.8f;
+			fixtureDef.restitution=0.15f;
+			Body body=box2dWorld.createBody(bodyDef);
+			body.createFixture(fixtureDef);
+			bodies.add(body);
+			fixtureDef.shape=null;
+			shape.dispose();
+		}//end for
+		
+	}
+}
+>>>>>>> parent of b47b57b... Revert "core android"
--- core/src/com/stary/utils/Box2dUtil.java
+++ core/src/com/stary/utils/Box2dUtil.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.stary.utils;
 
 import com.badlogic.gdx.maps.MapObject;
@@ -264,3 +265,177 @@ public class Box2dUtil {
 //		}
 //	}
 }
+=======
+package com.stary.utils;
+
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.CircleMapObject;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.PolylineMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.ChainShape;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.utils.Array;
+import com.esotericsoftware.spine.Bone;
+import com.esotericsoftware.spine.Skeleton;
+import com.esotericsoftware.spine.Slot;
+import com.esotericsoftware.spine.attachments.Attachment;
+import com.stary.data.GameData;
+
+public class Box2dUtil {
+	public static Shape getShape(MapObject mapObject,float pxToPhy){
+		Shape shape = null;
+		if (mapObject instanceof TextureMapObject) {
+			
+		}else if(mapObject instanceof RectangleMapObject){
+			shape=getRectangle((RectangleMapObject)mapObject,pxToPhy);
+		}else if(mapObject instanceof CircleMapObject){
+			shape=getCircle((CircleMapObject)mapObject,pxToPhy);
+		}else if(mapObject instanceof PolygonMapObject){
+			shape=getPolygon((PolygonMapObject)mapObject,pxToPhy);
+		}else if(mapObject instanceof PolylineMapObject){
+			shape=getPolyline((PolylineMapObject)mapObject,pxToPhy);
+		}
+		return shape;
+	}
+	public static float[] getVertices(MapObject mapObject,float pxToPhy){
+		float[] vs = null;
+		if (mapObject instanceof TextureMapObject) {
+			
+		}else if(mapObject instanceof RectangleMapObject){
+		}else if(mapObject instanceof CircleMapObject){
+		}else if(mapObject instanceof PolygonMapObject){
+		}else if(mapObject instanceof PolylineMapObject){
+			vs=getPolylineVertices((PolylineMapObject)mapObject,pxToPhy);
+		}
+		return vs;
+	}
+	public static Shape getRectangle(RectangleMapObject rectangleMapObject,float pxToPhy){
+		Vector2 center=new Vector2();
+		Rectangle rect=rectangleMapObject.getRectangle();
+		rect.getCenter(center);
+//		center.set(rect.getX(), rect.getY());
+		center.scl(pxToPhy);
+		
+		float r=rectangleMapObject.getProperties().get("rotation", 0f, Float.class);
+		System.out.println("rotation "+r);
+//		float cos=MathUtils.cosDeg(r);
+//		float sin=MathUtils.cosDeg(r);
+//		center.rotate(-r);
+		PolygonShape polygonShape=new PolygonShape();
+		//fixme TODO 搞不定
+		r=0;
+		polygonShape.setAsBox(rect.width*0.5f*pxToPhy, rect.height*0.5f*pxToPhy, center, -r*MathUtils.degreesToRadians);
+		return polygonShape;
+	}
+	public static Shape getCircle(CircleMapObject circleMapObject,float pxToPhy){
+		Circle c=circleMapObject.getCircle();
+		Vector2 pos=new Vector2(c.x*pxToPhy, c.y*pxToPhy);
+		CircleShape circleShape=new CircleShape();
+		circleShape.setPosition(pos);
+		circleShape.setRadius(c.radius*pxToPhy);
+		return circleShape;
+	}
+	public static Shape getPolygon(PolygonMapObject polygonMapObject,float pxToPhy){
+		float[] vertices=polygonMapObject
+				.getPolygon().getTransformedVertices();
+		float[] meterVertices=new float[vertices.length];
+		for (int i = 0; i < vertices.length; i++) {
+			meterVertices[i]=vertices[i]*pxToPhy;
+		}
+		PolygonShape polygonShape=new PolygonShape();
+		polygonShape.set(meterVertices);
+		return polygonShape;
+	}
+	public static float[] getPolylineVertices(PolylineMapObject polylineMapObject,float pxToPhy){
+		float[] vertices=polylineMapObject
+				.getPolyline().getTransformedVertices();
+//		System.out.println(polylineMapObject.getPolyline().getRotation());
+		float[] meterVertices=new float[vertices.length];
+		for (int i = 0; i < vertices.length; i++) {
+			meterVertices[i]=vertices[i]*pxToPhy;
+		}
+		return meterVertices;
+	}
+	public static Shape getPolyline(PolylineMapObject polylineMapObject,float pxToPhy){
+		float[] vertices=polylineMapObject
+				.getPolyline().getTransformedVertices();
+//		System.out.println(polylineMapObject.getPolyline().getRotation());
+		float[] meterVertices=new float[vertices.length];
+		for (int i = 0; i < vertices.length; i++) {
+			meterVertices[i]=vertices[i]*pxToPhy;
+		}
+		ChainShape chainShape=new ChainShape();
+		chainShape.createChain(meterVertices);
+		return chainShape;
+	}
+	public static void box2dToSpine(Array<Slot> slots){
+		for (Slot slot : slots) {
+			if (!(slot.getAttachment() instanceof Box2dBoundingBoxAttachment)) continue;
+			Box2dBoundingBoxAttachment attachment = (Box2dBoundingBoxAttachment)slot.getAttachment();
+			if (attachment.body == null) continue;
+			Body body=attachment.body;
+			Skeleton skeleton=slot.getSkeleton();
+			Bone bone=slot.getBone();
+			Vector2 pos=body.getPosition();//game world
+			pos.sub(skeleton.getX(),skeleton.getY());//->skeleton world
+			slot.getBone().getParent().worldToLocal(pos);//skl world -> local world
+			bone.setPosition(pos.x, pos.y);
+			float worldRotation=body.getAngle()*MathUtils.radDeg;//rad
+//			worldRotation=worldRotation-skeleton.getRootBone().
+			float localRotation=worldRotation-bone.getParent().getWorldRotation();
+			bone.setRotation(localRotation);
+			bone.updateWorldTransform();
+		}
+	}
+	/**
+	 * spine 2 box2d 按spine skeleton所有boundingbox的位置/角度给与box2d的body
+	 * @param slots
+	 */
+	public static void spineToBox2d(Array<Slot> slots){
+		for (Slot slot : slots) {
+			if (!(slot.getAttachment() instanceof Box2dBoundingBoxAttachment)) continue;
+			Box2dBoundingBoxAttachment attachment = (Box2dBoundingBoxAttachment)slot.getAttachment();
+			if (attachment.body == null) continue;
+			Skeleton skeleton=slot.getSkeleton();
+			float x = skeleton.getX() + slot.getBone().getWorldX();
+			float y = skeleton.getY() + slot.getBone().getWorldY();
+			float rotation = slot.getBone().getWorldRotation();
+//			System.out.println("body "+x+" "+y);
+			attachment.body.setTransform(x,y,rotation*MathUtils.degRad);
+		}
+	}
+	/**
+	 * 
+	 * @param line 地平线的顶点数组{x0,y0,x1,y1,x2,y2....} world coordinates
+	 * @param x	角色所处的x坐标(world coordinates
+	 * @return 地平线的Y坐标
+	 */
+	public static Float getLandY(float[] line,float x){
+		for (int i = 0; i < line.length; i=i+2) {
+			float lineX=line[i];
+			if (i<line.length-2 && lineX<=x && x<line[i+2]) {
+				float lineY=line[i+1];
+				float line2X=line[i+2];
+				float line2Y=line[i+3];
+				float dx=line2X-lineX;
+				float dy=line2Y-lineY;
+				//dy/dx=?/x
+				float newY=dy*(x-lineX)/dx;
+				return newY+lineY;
+			}
+		}
+		return null;
+	}
+}
+>>>>>>> parent of b47b57b... Revert "core android"
